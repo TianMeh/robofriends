20ebdc7f9e0159be2413ba3eae5406c1c8b87dd8
Tian Meh <tian.meh99@gmail.com>
2022-11-22 07:37:40 +0100

    removed border

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -1,6 +1,6 @@
 const Scroll = (props) => {
     return (
-        <div style={{overflowY: 'scroll', border: '5px solid black', height: '500px'}}>
+        <div style={{overflowY: 'scroll', height: '500px'}}>
             {props.children}
         </div>
     )
